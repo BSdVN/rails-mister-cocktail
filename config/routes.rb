5c5aa6dddd7df9c26c889535b3279c95fb3d7422
Beltran Sainz de Vicuna Nowack <gorthor@gmail.com>
2018-11-08 17:17:35 +0100

    work in doses/controller + html

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :new, :create, :show]
-  resources :doses, only: [:new, :create, :destroy]
 
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
   # get  "cocktails",     to: "cocktails#index"
-
   # get  "cocktails/new", to: "cocktails#new"
   # post "cocktails",     to: "cocktails#create"
-
   # get  "cocktails/:id", to: "cocktails#show"
-
   # get    "doses/new", to: "doses#new"
   # post   "doses",     to: "doses#create"
-
   # delete "doses/:id", to: "doses#destroy"
 end
