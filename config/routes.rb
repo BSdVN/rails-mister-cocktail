99e10eb29173edd60018305d2032a009fc7533fe
Beltran Sainz de Vicuna Nowack <gorthor@gmail.com>
2018-11-09 15:22:50 +0100

    need to seed doses... need to style html

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :new, :create, :show]
-
-  resources :cocktails do
+  resources :cocktails, only: [:index, :new, :create, :show] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
+
   # get  "cocktails",     to: "cocktails#index"
   # get  "cocktails/new", to: "cocktails#new"
   # post "cocktails",     to: "cocktails#create"
@@ -13,4 +12,5 @@ Rails.application.routes.draw do
   # get    "doses/new", to: "doses#new"
   # post   "doses",     to: "doses#create"
   # delete "doses/:id", to: "doses#destroy"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 require 'json'
+
+json = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+
+ingredients = JSON.parse(json)
+
+ingredients['drinks'].each do |ingredient|
+  name = ingredient['strIngredient1']
+  Ingredient.create(name: name)
+end
+
+json = open('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic
+').read
+
+cocktails = JSON.parse(json)
+
+cocktails['drinks'].each do |cocktail|
+  name = cocktail['strDrink']
+  Cocktail.create(name: name)
+end
